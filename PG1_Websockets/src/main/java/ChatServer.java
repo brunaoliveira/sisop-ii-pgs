ca1e1b08533dd06f1680dff04e835e399053a0be
brunaoliveira <brunaol678@gmail.com>
2021-10-14 19:02:56 -0300

    Working application

--- PG1_Websockets/src/main/java/ChatServer.java
+++ PG1_Websockets/src/main/java/ChatServer.java
@@ -1,20 +1,41 @@
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Logger;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 import javax.websocket.server.ServerEndpoint;
 import javax.websocket.Session;
-import java.util.logging.Logger;
 
 @ServerEndpoint("/chat")
 public class ChatServer {
-    private static final Logger LOGGER = Logger.getLogger(ChatServer.class.getName());
-    private static List<Session> openSessions = new ArrayList<Session>();
+    private static final Logger logger = Logger.getLogger(ChatServer.class.getName());
+    private static List<Session> sessions = new ArrayList<Session>();
 
-    @onOpen
-    public void openSession(Session new_session) {
-    
+    @OnOpen
+    public void openSession(Session newSession) {
+        if (sessions.add(newSession)) {
+            logger.info("New session opened: " + newSession.getId());
+        }
+        else {
+            logger.warning("Session was already being tracked: " + newSession.getId());
+        }
     } 
 
-    @onClose
-    public void closeSession(Session closing_session) {
-        
+    @OnClose
+    public void closeSession(Session closingSession) {
+        if (sessions.remove(closingSession)) {
+            logger.info("Session closed: " + closingSession.getId());
+        }
+        else {
+            logger.warning("Session is not being tracked: " + closingSession.getId());
+        }
     }
 
+    @OnMessage
+    public void sendMessage(String message) throws IOException {
+        for(Session session : sessions) {
+            session.getBasicRemote().sendText(message);
+        }
+    }
 }
